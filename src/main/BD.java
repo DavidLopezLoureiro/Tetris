69c36905f9368626d414ba37678b15798a0d652e
david <david@LAPTOP-9TD9V43L>
2019-10-22 20:08:30 +0200

    BD entrada de datos

--- src/main/BD.java
+++ src/main/BD.java
@@ -1,7 +1,29 @@
 package main;
 
+import java.util.Scanner;
+
 public class BD{
 
-	// aqui va la base de datos 
+	public static void main(String[] args) {
+		
+	//LECTURA DE ENTRADA DE DATOS
+		
+	
+	Scanner entrada = new Scanner(System.in);
+
+	System.out.print("Escribe el usuario: ");
+
+	String usuario = entrada.nextLine();
+
+	System.out.println("\n ");
+
+	System.out.print("Escribe la contraseña: ");
+
+	String contraseña = entrada.nextLine();
+	
+	entrada.close();	
+	
+	
+	}
 	
 }
