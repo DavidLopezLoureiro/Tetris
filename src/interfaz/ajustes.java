fa39bb97f16850188d5ac0353f8874879270f874
Xabier <Xabier@LAPTOP-SATF7FP1>
2019-11-14 15:24:27 +0100

    Interfaz ajustes

--- src/interfaz/ajustes.java
+++ src/interfaz/ajustes.java
@@ -11,6 +11,11 @@ import javax.swing.JLabel;
 import javax.swing.JButton;
 
 import javax.swing.JSlider;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.JRadioButton;
 
 @SuppressWarnings("serial")
 public class ajustes extends JFrame {
@@ -38,46 +43,89 @@ public class ajustes extends JFrame {
 	 */
 	public ajustes() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 685, 413);
+		setBounds(100, 100, 850, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblVolumen = new JLabel("Volumen:");
-		lblVolumen.setBounds(12, 37, 99, 32);
-		contentPane.add(lblVolumen);
-		
 		JSlider slider = new JSlider();
-		slider.setBounds(12, 82, 625, 26);
+		slider.setBounds(88, 222, 672, 26);
 		contentPane.add(slider);
 		
-		JLabel lblDificultad = new JLabel("Dificultad:");
-		lblDificultad.setBounds(12, 132, 83, 16);
-		contentPane.add(lblDificultad);
-		
-		JSlider slider_1 = new JSlider();
-		slider_1.setBounds(12, 161, 625, 26);
-		contentPane.add(slider_1);
-		
-		JButton btnReiniciar = new JButton("Reiniciar");
-		btnReiniciar.setBounds(12, 210, 170, 65);
+		JButton btnReiniciar = new JButton("REINICIAR");
+		btnReiniciar.setForeground(new Color(255, 255, 255));
+		btnReiniciar.setBackground(new Color(0, 191, 255));
+		btnReiniciar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnReiniciar.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		btnReiniciar.setBounds(65, 546, 292, 65);
 		contentPane.add(btnReiniciar);
 		
-		JButton btnReanudar = new JButton("Reanudar");
-		btnReanudar.setBounds(88, 288, 472, 65);
+		JButton btnReanudar = new JButton("RENAUDAR");
+		btnReanudar.setBackground(new Color(173, 255, 47));
+		btnReanudar.setForeground(new Color(255, 255, 255));
+		btnReanudar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnReanudar.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		btnReanudar.setBounds(273, 457, 292, 65);
 		contentPane.add(btnReanudar);
 		
-		JButton btnGuardarYSalir = new JButton("Guardar y salir");
-		btnGuardarYSalir.setBounds(467, 210, 170, 65);
+		JButton btnGuardarYSalir = new JButton("GUARDAR Y SALIR");
+		btnGuardarYSalir.setBackground(new Color(0, 191, 255));
+		btnGuardarYSalir.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		btnGuardarYSalir.setBounds(468, 546, 292, 65);
 		contentPane.add(btnGuardarYSalir);
 		
-		JButton btnCerrarSesion = new JButton("Cerrar sesion");
-		btnCerrarSesion.setBounds(216, 210, 219, 65);
-		contentPane.add(btnCerrarSesion);
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(220, 220, 220));
+		panel.setForeground(new Color(220, 220, 220));
+		panel.setBounds(0, 0, 832, 115);
+		contentPane.add(panel);
+		panel.setLayout(null);
 		
 		JLabel lblAjustes = new JLabel("AJUSTES");
-		lblAjustes.setBounds(296, 13, 68, 32);
-		contentPane.add(lblAjustes);
+		lblAjustes.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 35));
+		lblAjustes.setBounds(327, 34, 153, 46);
+		panel.add(lblAjustes);
+		
+		JLabel lblVolumen = new JLabel("VOLUMEN:");
+		lblVolumen.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblVolumen.setBounds(88, 160, 127, 16);
+		contentPane.add(lblVolumen);
+		
+		JLabel lblDificultad = new JLabel("DIFICULTAD:");
+		lblDificultad.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		lblDificultad.setBounds(88, 285, 154, 16);
+		contentPane.add(lblDificultad);
+		
+		JRadioButton rdbtnFcil = new JRadioButton("F\u00C1CIL");
+		rdbtnFcil.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		rdbtnFcil.setBounds(88, 345, 127, 25);
+		contentPane.add(rdbtnFcil);
+		
+		JRadioButton rdbtnMedio = new JRadioButton("MEDIO");
+		rdbtnMedio.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		rdbtnMedio.setBounds(374, 345, 127, 25);
+		contentPane.add(rdbtnMedio);
+		
+		JRadioButton rdbtnDifcil = new JRadioButton("DIF\u00CDCIL");
+		rdbtnDifcil.setFont(new Font("Times New Roman", Font.PLAIN, 18));
+		rdbtnDifcil.setBounds(654, 345, 93, 25);
+		contentPane.add(rdbtnDifcil);
+		
+		JLabel label = new JLabel("-");
+		label.setFont(new Font("Times New Roman", Font.PLAIN, 30));
+		label.setBounds(65, 204, 56, 16);
+		contentPane.add(label);
+		
+		JLabel label_1 = new JLabel("+");
+		label_1.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		label_1.setBounds(764, 205, 56, 16);
+		contentPane.add(label_1);
 	}
 }
