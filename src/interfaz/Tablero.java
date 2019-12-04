1558f2a96e7ce7d72fbd644376dc7c313fcd5d74
david <david@192.168.1.161>
2019-12-04 17:49:34 +0100

    Cambio de pintado

--- src/interfaz/Tablero.java
+++ src/interfaz/Tablero.java
@@ -3,21 +3,11 @@ import java.awt.Graphics;
 
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
+
+
 import java.awt.Color;
 
 public class Tablero extends JPanel {
-	
-	public static int y_0;
-	public static int x_0;
-	
-	public static int y_1;
-	public static int x_1;
-	
-	public static int y_2;
-	public static int x_2;
-	
-	public static int y_3;
-	public static int x_3;
 	
 	/**
 	 * 
@@ -34,42 +24,6 @@ public class Tablero extends JPanel {
 	public void paint(Graphics arg0) {
 		
 		super.paint(arg0);
-		//pintamos las diferentes lineas del tablero
-		//lineas horizontales
-		arg0.drawLine(0,30,270,30 );
-		arg0.drawLine(0,60,270,60 );
-		arg0.drawLine(0,90,270,90 );
-		arg0.drawLine(0,120,270,120 );
-		arg0.drawLine(0,150,270,150 );
-		arg0.drawLine(0,180,270,180 );
-		arg0.drawLine(0,210,270,210 );
-		arg0.drawLine(0,240,270,240 );
-		arg0.drawLine(0,270,270,270 );
-		arg0.drawLine(0,300,270,300 );
-		arg0.drawLine(0,330,270,330 );
-		arg0.drawLine(0,360,270,360 );
-		arg0.drawLine(0,390,270,390 );
-		arg0.drawLine(0,420,270,420 );
-		arg0.drawLine(0,450,270,450 );
-		arg0.drawLine(0,480,270,480 );
-		arg0.drawLine(0,510,270,510 );
-		arg0.drawLine(0,540,270,540 );
-		arg0.drawLine(0,570,270,570 );
-		arg0.drawLine(0,600,270,600 );
-		arg0.drawLine(0,630,270,630 );
-		//lineas verticales
-		arg0.drawLine(0,0,0,630 );
-		arg0.drawLine(30,0,30,630 );
-		arg0.drawLine(60,0,60,630 );
-		arg0.drawLine(90,0,90,630 );
-		arg0.drawLine(120,0,120,630 );
-		arg0.drawLine(150,0,150,630 );
-		arg0.drawLine(180,0,180,630 );
-		arg0.drawLine(210,0,210,630 );
-		arg0.drawLine(240,0,240,630 );
-		arg0.drawLine(270,0,270,630 );
-		arg0.drawLine(300,0,300,630 );
-		
 		
 		//PINTADO DEL CAMPO
 	for(int i = 0 ; i < 21; i++) {
@@ -113,10 +67,6 @@ public class Tablero extends JPanel {
 			}
 		}
 
-
-
-
-
 		//PINTA LA PIEZA EN MOVIMIENTO
 			if(Ventana.Cod_pieza_actual == "T") {
 				
@@ -159,6 +109,50 @@ public class Tablero extends JPanel {
 			arg0.fillRect(Ventana.x_2, Ventana.y_2, 30, 30);
 			arg0.fillRect(Ventana.x_3, Ventana.y_3, 30, 30);
 		
+			///LINEAS DEL CAMPO
+			
+			//lineas horizontales
+			arg0.setColor(Color.BLACK);
+			arg0.drawLine(0,30,270,30 );
+			arg0.drawLine(0,60,270,60 );
+			arg0.drawLine(0,90,270,90 );
+			arg0.drawLine(0,120,270,120 );
+			arg0.drawLine(0,150,270,150 );
+			arg0.drawLine(0,180,270,180 );
+			arg0.drawLine(0,210,270,210 );
+			arg0.drawLine(0,240,270,240 );
+			arg0.drawLine(0,270,270,270 );
+			arg0.drawLine(0,300,270,300 );
+			arg0.drawLine(0,330,270,330 );
+			arg0.drawLine(0,360,270,360 );
+			arg0.drawLine(0,390,270,390 );
+			arg0.drawLine(0,420,270,420 );
+			arg0.drawLine(0,450,270,450 );
+			arg0.drawLine(0,480,270,480 );
+			arg0.drawLine(0,510,270,510 );
+			arg0.drawLine(0,540,270,540 );
+			arg0.drawLine(0,570,270,570 );
+			arg0.drawLine(0,600,270,600 );
+			arg0.drawLine(0,630,270,630 );
+			//lineas verticales
+			arg0.drawLine(0,0,0,630 );
+			arg0.drawLine(30,0,30,630 );
+			arg0.drawLine(60,0,60,630 );
+			arg0.drawLine(90,0,90,630 );
+			arg0.drawLine(120,0,120,630 );
+			arg0.drawLine(150,0,150,630 );
+			arg0.drawLine(180,0,180,630 );
+			arg0.drawLine(210,0,210,630 );
+			arg0.drawLine(240,0,240,630 );
+			arg0.drawLine(270,0,270,630 );
+			arg0.drawLine(300,0,300,630 );
+
+
+
+
+
+			
+
 		repaint();
 	
 		
