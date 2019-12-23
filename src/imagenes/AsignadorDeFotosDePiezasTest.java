86639f9ea367eaf2c628442a95012de92005783f
erik <erikmartinez0910@gmail.com>
2019-12-23 17:59:10 +0100

    Test del asignador de archivos

---
+++ src/imagenes/AsignadorDeFotosDePiezasTest.java
@@ -0,0 +1,39 @@
+package imagenes;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AsignadorDeFotosDePiezasTest {
+	//inicializamos previamente un arraylist con todos los tipos de piezas parr la asignacion
+	ArrayList<String> tipos;
+	@Before
+	public void initialize() {
+		tipos=new ArrayList<String>();
+		tipos.add("I");
+		tipos.add("L");
+		tipos.add("O");
+		tipos.add("R");
+		tipos.add("S");
+		tipos.add("T");
+		tipos.add("Z");
+	}
+//	comprobamos que las asignaciones de imagagenes se estan haciendo correctamente
+	@Test
+	public void AsignacionFotos() {
+		for ( String dato: tipos) {
+			String prueba="/imagenes/"+dato+".png";
+			if(prueba.equals(AsignadorDeFotosDePiezas.poner_foto(dato))) {
+				assertTrue(true);
+			}else {
+				assertTrue(false);
+			}
+			
+		}
+		
+	}
+
+}
---
+++ src/objetos/CuadradoTest.java
@@ -0,0 +1,14 @@
+package objetos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CuadradoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
