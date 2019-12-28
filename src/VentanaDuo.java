c787bf0b10a019e5398ed39d75773439255323df
Xabier <Xabier@LAPTOP-SATF7FP1>
2019-12-28 19:11:56 +0100

    Nuevo modo de juego (inicio: Interfaz)

---
+++ src/VentanaDuo.java
@@ -0,0 +1,27 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+import interfaz.ElegirModo;
+
+public class VentanaDuo extends JFrame{
+	
+private JPanel contentPane;
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					VentanaDuo frame = new VentanaDuo();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+
+}
