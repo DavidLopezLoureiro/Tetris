a47fe4bf2c5fa5e7e80262bb9dd2ec454007896c
Xabier <Xabier@LAPTOP-SATF7FP1>
2019-12-29 18:14:28 +0100

    Interfaz de ElejirModo.java acabada

--- src/interfaz/ElegirModo.java
+++ src/interfaz/ElegirModo.java
@@ -7,6 +7,10 @@ import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ElegirModo extends JFrame{
 	public ElegirModo() {
@@ -17,25 +21,40 @@ public class ElegirModo extends JFrame{
 		
 		getContentPane().setLayout(null);
 		
-		JButton btnNewButton = new JButton("CLASICO");
-		btnNewButton.setFont(new Font("Times New Roman", Font.BOLD, 25));
-		btnNewButton.setBounds(60, 140, 351, 154);
-		getContentPane().add(btnNewButton);
+		JButton botonModoClasico = new JButton("CLASICO");
+		botonModoClasico.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		botonModoClasico.setBounds(103, 207, 242, 97);
+		getContentPane().add(botonModoClasico);
 		
-		JButton btnJugadores = new JButton("2 JUGADORES");
-		btnJugadores.setFont(new Font("Times New Roman", Font.BOLD, 25));
-		btnJugadores.setBounds(60, 418, 351, 154);
-		getContentPane().add(btnJugadores);
+		JButton botonModoDuo = new JButton("2 JUGADORES");
+		botonModoDuo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		botonModoDuo.setFont(new Font("Times New Roman", Font.BOLD, 25));
+		botonModoDuo.setBounds(103, 468, 242, 97);
+		getContentPane().add(botonModoDuo);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon(ElegirModo.class.getResource("/imagenes/961570e90e488f080c2a3be82a3ea02a_icon.jpg")));
-		lblNewLabel.setBounds(501, 86, 256, 251);
-		getContentPane().add(lblNewLabel);
+		JLabel labelSegundaImagen = new JLabel("");
+		labelSegundaImagen.setIcon(new ImageIcon(ElegirModo.class.getResource("/imagenes/c4400e39015d750887475facd79b3e42.png")));
+		labelSegundaImagen.setBounds(497, 135, 292, 250);
+		getContentPane().add(labelSegundaImagen);
 		
-		JLabel label = new JLabel("");
-		label.setIcon(new ImageIcon(ElegirModo.class.getResource("/imagenes/descarga1.png")));
-		label.setBounds(501, 363, 285, 251);
-		getContentPane().add(label);
+		JLabel labelPrimeraImagen = new JLabel("");
+		labelPrimeraImagen.setIcon(new ImageIcon(ElegirModo.class.getResource("/imagenes/trophy-256x256.png")));
+		labelPrimeraImagen.setBounds(497, 412, 250, 228);
+		getContentPane().add(labelPrimeraImagen);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(220, 220, 220));
+		panel.setBounds(0, 0, 844, 105);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel labelTitulo = new JLabel("JUEGO");
+		labelTitulo.setFont(new Font("Times New Roman", Font.BOLD | Font.ITALIC, 35));
+		labelTitulo.setBounds(358, 29, 128, 46);
+		panel.add(labelTitulo);
 	}
 
 	
