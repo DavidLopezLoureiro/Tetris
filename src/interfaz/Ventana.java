eab7b6cda5279f5874f83ce0182b0f886799d5e8
david <david@LAPTOP-DLL>
2020-01-12 23:26:52 +0100

    musica parte 1

--- src/interfaz/Ventana.java
+++ src/interfaz/Ventana.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Font;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.swing.ImageIcon;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -337,6 +339,28 @@ public class Ventana extends JFrame implements KeyListener {
 	 */
 	public Ventana() {
 		
+		
+		try {
+            
+            // Se obtiene un Clip de sonido
+            Clip sonido = AudioSystem.getClip();
+            
+            // Se carga con un fichero wav
+            sonido.open(AudioSystem.getAudioInputStream(getClass().getResourceAsStream("/imagenes/WhatsApp Audio 2020-01-12 at 21.42.46 (1).wav")));
+            
+            // Comienza la reproducción
+            sonido.start();
+            
+            // Espera mientras se esté reproduciendo.
+            while (sonido.isRunning())
+                Thread.sleep(1000);
+            
+            // Se cierra el clip.
+            
+        } catch (Exception e) {
+            System.out.println("" + e);
+        }
+
 		
 		//aÃ±adimos dificultad
 		anyadirDificultad();
